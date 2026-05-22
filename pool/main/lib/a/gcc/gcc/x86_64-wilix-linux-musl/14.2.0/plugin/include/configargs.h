d639426a610a1871721c8cd7fdc9bcfeb926a5ed
просто человек <nuhajjuh@gmail.com>
2026-05-22 13:58:35 +0300

    Delete pool/main/lib/a/gcc/gcc/x86_64-wilix-linux-musl/14.2.0 directory

--- pool/main/lib/a/gcc/gcc/x86_64-wilix-linux-musl/14.2.0/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../gcc-14.2.0/configure --prefix=/usr --target=x86_64-wilix-linux-musl --enable-languages=c,c++ --disable-multilib --disable-bootstrap --disable-libsanitizer --disable-nls --disable-cet";
-static const char thread_model[] = "posix";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
